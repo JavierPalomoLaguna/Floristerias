e7dc7ec7eaa09585369dc5dce65f03e850a1950a
Javier Palomo <jpalomolaguna@gmail.com>
2025-12-11 13:26:52 +0100

    feat: cambio cuenta email en politica de privacidad

--- proyectoweb/settings.py
+++ proyectoweb/settings.py
@@ -166,18 +166,6 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
-
-'''
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jpalomolaguna@gmail.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-'''
-
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
