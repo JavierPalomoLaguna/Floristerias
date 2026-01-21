93ec9db3b3dcc42e794179c7ab2d380b2befc1b6
Javier Palomo <jpalomolaguna@gmail.com>
2026-01-21 17:03:05 +0100

    cambio urls.py para mejora de seo

--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from ProyectoWebApp import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('home/', views.home, name='home'),
+    path('home/', lambda request: redirect('/', permanent=True)),
     path('politica-privacidad/', views.politica_privacidad, name='politica_privacidad'),
     path('aviso-legal/', views.aviso_legal, name='aviso_legal'),
 ]
